e3aba6828585cf2a6afdd421dd304fef9f493ce2
Alex Yang <ayang41@gmail.com>
2021-05-12 13:25:22 -0400

    Slow down scraping frequency

--- colonial/app.py
+++ colonial/app.py
@@ -1,3 +1,4 @@
+import logging
 import time
 import threading
 import streamlit as st
@@ -7,11 +8,20 @@ import pandas as pd
 from colonial.data import get_s3filesystem, GASBUDDY_BUCKET
 from colonial.scraper import state_scraper, us_scraper, station_scraper
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(levelname)s:%(asctime)s:%(message)s',
+    level=logging.DEBUG
+)
+
 def main():
+    logger.debug("Launching main app")
     st.title("Gas buddy interactive chart")
+    st.text("Costco gas stations are in VA")
     
     df = load_data()
     
+    logger.debug("Creating chart")
     chart = alt.Chart(df).mark_line(point=True).encode(
             x='Timestamp:T',
             y=alt.Y('Price:Q', scale=alt.Scale(zero=False)),
@@ -23,10 +33,24 @@ def main():
         height=500
     ).interactive()
     
-    st.altair_chart(chart, use_container_width=True)
+    mychart = st.altair_chart(chart, use_container_width=True)
+    
+    while True:
+        logger.debug("Launching scraping threads")
+        t1 = state_scrape_thread()
+        t2 = us_scrape_thread()
+        t3 = station_scrape_thread()
+
+        t1.join()
+        t2.join()
+        t3.join()
+        logger.debug("Adding rows to chart")
+        mychart.add_rows(load_data())
+        time.sleep(10 * 60)
 
 
 def load_data():
+    logger.debug("Loading data from S3")
     csvfiles = ['station_data.csv', 'state_data.csv', 'us_data.csv']
     s3 = get_s3filesystem()
     all_dfs = []
@@ -41,24 +65,25 @@ def load_data():
 
 def state_scrape_thread():
     state_thread = threading.Thread(target=state_scraper)
+    st.report_thread.add_report_ctx(state_thread)
     state_thread.start()
-    state_thread.join()
-    time.sleep(30)
+
+    return state_thread
 
 
 def us_scrape_thread():
     us_thread = threading.Thread(target=us_scraper)
+    st.report_thread.add_report_ctx(us_thread)
     us_thread.start()
-    us_thread.join()
-    time.sleep(30)
 
+    return us_thread
 
 def station_scrape_thread():
     station_thread = threading.Thread(target=station_scraper)
+    st.report_thread.add_report_ctx(station_thread)
     station_thread.start()
-    station_thread.join()
-    time.sleep(30)
 
+    return station_thread
 
 if __name__ == "__main__":
     main()
